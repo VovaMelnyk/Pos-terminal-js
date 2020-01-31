5ef15dfa2f8335c93e70faf0cf5de31e6811a729
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2020-01-31 22:53:14 +0200

    add js folder

---
+++ src/js/example.js
@@ -0,0 +1,38 @@
+// import moment from 'moment'; // add library
+// import M from 'materialize-css'; // add materialize js logic
+// import '@/styles/materialize/materialize'; // import materialize styles
+// import '@/styles/base'; // add you own styles
+
+// import imgSource from '@/assets/img/javascript.jpg'; // add image to you markup
+
+// export function example() {
+//   const root = document.querySelector('#root');
+//   const img = document.createElement('img');
+//   img.src = imgSource;
+//   root.append(img);
+// }
+
+// export function example2() {
+//   const root = document.querySelector('#root');
+//   const img = `<img src=${imgSource} alt='img'/>`;
+//   root.innerHTML = img;
+// }
+
+// export function materializeExample() {
+//   const root = document.querySelector('#root');
+//   root.innerHTML = ` <a class="waves-effect waves-light btn modal-trigger" href="#modal1">Modal</a>
+// <div id="modal1" class="modal">
+//   <div class="modal-content">
+//     <h4>Modal Header</h4>
+//     <p>A bunch of text</p>
+//   </div>
+//   <div class="modal-footer">
+//     <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+//   </div>
+// </div>`;
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.modal');
+//     var instances = M.Modal.init(elems);
+//   });
+// }
