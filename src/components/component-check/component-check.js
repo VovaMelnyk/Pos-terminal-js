1f3b5b0437c7a05b9f41bbfc74e9480258a13099
Alexey <valnistuy@gmail.com>
2020-02-02 13:50:21 +0200

    added renderButtons and renderComment method

--- src/components/component-check/component-check.js
+++ src/components/component-check/component-check.js
@@ -1,4 +1,5 @@
 import '@/styles/materialize/materialize';
+import M from 'materialize-css';
 import './component-check-style.scss';
 
 class Check {
@@ -21,12 +22,16 @@ class Check {
 
   init(container) {
     this.addToScreen(container, 'beforeend', this.renderCheck());
+
+    this.initDropdown();
   }
 
   renderCheck() {
     return `
       <div class="check">
         ${this.renderList()}
+        ${this.renderComments()}
+        ${this.renderCheckButtons()}
       </div>`;
   }
 
@@ -36,8 +41,8 @@ class Check {
         <table>
           <thead>
             <tr>
-              <th>Наименование</th>
-              <th>Кол-во</th>
+              <th class="title">Наименование</th>
+              <th class="quantity">Кол-во</th>
               <th>Цена</th>
               <th>Итого</th>
             </tr>
@@ -51,12 +56,53 @@ class Check {
 
   renderListItem({ title, quantity, price }) {
     return `<tr class="food-list__item">
-              <td>${title}</td>
-              <td>${quantity}</td>
+              <td class="title">${title}</td>
+              <td class="quantity">${quantity}</td>
               <td>${price.toFixed(2)}</td>
-              <td>${this.countingAmount(quantity, price)}</td>
+              <td class="total">${this.countingAmount(quantity, price)}</td>
             </tr>
           `;
+  }
+
+  renderComments() {
+    return `
+      <div class="comment">
+        <p><span class="bold">Комментарий:</span> 1111</p>
+      </div>`;
+  }
+
+  
+
+  renderCheckButtons() {
+    return `
+      <div class="check-buttons">
+        <button class="dropdown-trigger btn more" data-target='dropdown1'>
+          <i class="material-icons">more_horiz</i>
+        </button>
+        ${this.renderDropdownMenu()}
+        <button class="btn pay">Оплатить</button>
+      </div>`;
+  }
+
+  renderDropdownMenu() {
+    return `
+      <ul id='dropdown1' class='dropdown-content'>
+        <li><a href="#!">Комментарий к чеку...</a></li>
+        <li><a href="#!">Очистить заказ</a></li>
+      </ul>
+    `;
+  }
+
+  initDropdown() {
+    document.addEventListener('DOMContentLoaded', () => {
+      const elems = document.querySelectorAll('.dropdown-trigger');
+      const options = {
+        closeOnClick: false,
+        constrainWidth: false
+      };
+
+      M.Dropdown.init(elems, options);
+    });
   }
 
   addToScreen(container, position, element) {
