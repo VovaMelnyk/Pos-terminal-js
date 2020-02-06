da78fad270e16e033a51e246cee0d1a256e30134
Dmitry <53657951+dmytrokzmnk@users.noreply.github.com>
2020-02-06 12:33:27 +0200

    fixed error inline-style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // import { example, example2, materializeExample } from '@/js/example';
 
 // materializeExample();
+// import Admin from '@/js/admin-page/admin-page.js';
+// const admin = new Admin();
+// const root = document.querySelector('#root');
+// admin.start(root);
