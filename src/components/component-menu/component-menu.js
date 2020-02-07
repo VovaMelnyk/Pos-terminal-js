c22e9fdab6678b2ceefe19a5e8ef3c3639cbb40c
Alexey <valnistuy@gmail.com>
2020-02-07 23:01:45 +0200

    create categories-list, added renderCategoriesList and renderCategoriesItems, added style for categories-list

--- src/components/component-menu/component-menu.js
+++ src/components/component-menu/component-menu.js
@@ -1,9 +1,58 @@
+import Check from '../component-check/component-check';
+
 import '@/styles/materialize/materialize';
-import './component-check-style.scss';
+import './component-menu-style.scss';
 
 class Menu {
   constructor() {
-    this.categoriesList = [{}];
+    this.categoriesList = [
+      {
+        id: Date.now() + 1,
+        title: 'Первые блюда',
+        productList: [
+          {
+            id: Date.now() + 4,
+            title: 'Борщ',
+            quantity: 1,
+            price: 50,
+            img:
+              'https://img.povar.ru/main/ab/23/b4/9c/samii_vkusnii_borsh-404089.jpg',
+          },
+          {
+            id: Date.now() + 5,
+            title: 'Минестроне',
+            quantity: 1,
+            price: 120,
+            img:
+              'https://grandkulinar.ru/uploads/posts/2017-10/1509267258_vegetarianskij-minestrone.jpg',
+          },
+        ],
+        image:
+          'https://juicyworld.ru/wp-content/uploads/2019/05/sup-4-800x445.jpg',
+      },
+      {
+        id: Date.now() + 2,
+        title: 'Холодные закуски',
+        productList: [
+          {
+            id: Date.now() + 6,
+            title: 'Кольца кальмара',
+            quantity: 1,
+            price: 90,
+            img:
+              'https://buyseafood.ru/upload/iblock/d96/d9680d5219fc0860823a82c02b653aeb.jpg.pagespeed.ce.uCwi-YK08S.jpg',
+          },
+          {
+            id: Date.now() + 7,
+            title: 'Бутерброд с семгой',
+            quantity: 1,
+            price: 120,
+            img: 'https://i.ytimg.com/vi/cEALpGVB-cA/maxresdefault.jpg',
+          },
+        ],
+        image: 'https://galaktica.ru/images/newMenu-plitka/italia-kolbasa.jpg',
+      },
+    ];
   }
 
   init(container) {
@@ -16,7 +65,33 @@ class Menu {
 
   renderMenu() {
     return `
-      <div class="menu"></div>`;
+      <div class="menu">
+        ${this.renderCategoriesList()}
+      </div>`;
+  }
+
+  renderCategoriesList() {
+    return `
+      <ul class="categories-list">
+        ${this.categoriesList.reduce(
+          (acc, el) => this.renderCategoriesItems(el) + acc,
+          '',
+        )}
+      </ul>`;
+  }
+
+  renderCategoriesItems({ title, id, image }) {
+    return `
+      <li class="categories-list__item" data-categories-id="${id}">
+        <div class="card">
+          <div class="card-image">
+            <img src=${image} width="300" height="100">
+          </div>
+          <div class="card-content">
+            <p class="card-title">${title}</p>
+          </div>
+        </div>
+      </li>`;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,39 @@
 // import { example, example2, materializeExample } from '@/js/example';
-
+import './styles/base.scss';
 // materializeExample();
-
-/**
- * Code for review Menu component.
- */
-  import Menu from './components/component-menu/component-menu';
-
-  const menu = new Menu();
-
-  const root = document.querySelector('#root');
-
-  menu.init(root);
-/**
- * Code for review Menu component.
- */
+const root = document.querySelector('#root');
+createTestBox();
+const tb = document.querySelector('.testbox');
 
 /**
  * Code for review Check component.
  */
-// import Check from './components/component-check/component-check';
+import Check from './components/component-check/component-check';
 
-// const check = new Check();
+const check = new Check();
 
-// const root = document.querySelector('#root');
-
-// check.init(root);
+check.init(tb);
 /**
  * Code for review Check component.
  */
+
+/**
+ * Code for review Menu component.
+ */
+import Menu from './components/component-menu/component-menu';
+
+const menu = new Menu();
+
+menu.init(tb);
+/**
+ * Code for review Menu component.
+ */
+
+function createTestBox() {
+  const testBox = document.createElement('div');
+
+  testBox.classList.add('testbox');
+
+  root.append(testBox);
+
+}
