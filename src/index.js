58f748eec47e2c53435aa81324a1a0c1d48a58c0
Polina04 <baksheyeva0404@gmail.com>
2020-02-15 14:30:37 +0200

    initional commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // import { example, example2, materializeExample } from '@/js/example';
 
 // materializeExample();
+import menu from '@/components/menu/menu.js';
+const menuAdmin = new menu();
+const root = document.querySelector('#root');
+menuAdmin.start(root);
