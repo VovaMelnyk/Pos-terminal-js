28590eb1ba1fa097bda76ff07889f2a8edb04de9
IvanPustovit <pustovit.for@gmail.com>
2020-02-16 18:05:01 +0200

    ClassHoll1

--- src/hall.js
+++ src/hall.js
@@ -10,22 +10,54 @@ export default class Hall {
     renderHall() {
         return `
         <div class="container ">
-            <div class="table">1</div>
-            <div class="table">2</div>
-            <div class="table">3</div>
-            <div class="table">4</div>
-            <div class="table">5</div>
-            <div class="table">6</div>
-            <div class="table">7</div>
-            <div class="table">8</div>
-            <div class="table">9</div>
-            <div class="table">10</div>
-            <div class="table">11</div>
-            <div class="table">12</div>
-            <div class="table">13</div>
-            <div class="table">14</div>
-            <div class="table">15</div>
-            <div class="table">16</div>  
+            <div class="table">1
+            <span class="table_item"></span>
+            </div>
+            <div class="table">2
+            <span class="table_item"></span>
+            </div>
+            <div class="table">3
+            <span class="table_item"></span>
+            </div>
+            <div class="table">4
+            <span class="table_item"></span>
+            </div>
+            <div class="table">5
+            <span class="table_item"></span>
+            </div>
+            <div class="table">6
+            <span class="table_item"></span>
+            </div>
+            <div class="table">7
+            <span class="table_item"></span>
+            </div>
+            <div class="table">8
+            <span class="table_item"></span>
+            </div>
+            <div class="table">9
+            <span class="table_item"></span>
+            </div>
+            <div class="table">10
+            <span class="table_item"></span>
+            </div>
+            <div class="table">11
+            <span class="table_item"></span>
+            </div>
+            <div class="table">12
+            <span class="table_item"></span>
+            </div>
+            <div class="table">13
+            <span class="table_item"></span>
+            </div>
+            <div class="table">14
+            <span class="table_item"></span>
+            </div>
+            <div class="table">15
+            <span class="table_item"></span>
+            </div>
+            <div class="table">16
+            <span class="table_item"></span>
+            </div>  
         </div>`;
     }
 
@@ -64,12 +96,31 @@ return `<div class="list">
         let allGuest = (e.target.textContent)
 
         if(this.guest.length<0){
-            this.guest.push(+allGuest)
+            this.guest.push(+allGuest);
+    console.log(this.guest);
+
         }
         this.guest.splice(0,1,+allGuest)
-        console.log(this.guest);
+        // console.log(this.guest);
 
     }
+
+addClassBgr(e){
+    e.preventDefault()
+    if(e.target.tagName !== 'DIV') return;
+    console.log(e.target);
+    if(this.guest.length > 0){
+    e.target.classList.add('table_bgr')
+    } 
+
+}
+
+adds(){
+    const container = document.querySelector('.container');
+    container.addEventListener('click', this.addClassBgr.bind(this));
+
+}
+
 
     addLisnersGuest(){
         const container = document.querySelector('.container');
@@ -87,13 +138,14 @@ return `<div class="list">
 
     addLisners(){
         const container = document.querySelector('.container');
-        container.addEventListener('click', this.isnElement.bind(this));
+        container.addEventListener('click', this.isnElement.bind(this),this.addClassBgr.bind(this));
     }
         
     start(container){
         this.addToScreen(container, 'beforeend', this.renderHall());
         this.addLisners();
         this.addLisnersGuest()
+        this.adds()
         
     }
 }
