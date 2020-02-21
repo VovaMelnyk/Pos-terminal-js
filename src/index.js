a17ed7af452db7fd1e01f7d71c543b78bd5d328c
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-02-21 17:31:35 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // import { example, example2, materializeExample } from '@/js/example';
 
 // materializeExample();
+import GoodsCollection from './js/goodsCollection/goodsCollection';
+const collectionG = new GoodsCollection();
+const workShop = document.querySelector('#root');
+collectionG.start(workShop);
