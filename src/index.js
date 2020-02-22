7e934aed25399475cd958b89f7cdb65279ac02ca
NazarKinash <nazarkynash16@gmail.com>
2020-02-22 18:16:55 +0200

    fix

--- src/index.js
+++
@@ -1,7 +0,0 @@
-<<<<<<< HEAD
-// import { example, example2, materializeExample } from '@/js/example';
-import LoginForm from './js/login-form';
-new LoginForm();
-// materializeExample();
-=======
->>>>>>> dev
