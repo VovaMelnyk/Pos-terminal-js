504c50ef935599fd828e634648d6bd1e25a69d13
liliia-r <liliaglushko1992@gmail.com>
2020-02-22 14:19:29 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // import { example, example2, materializeExample } from '@/js/example';
-
+import NewCategoryIngredient from "./newCategoryIngredient/newCategoryIngredient";
 // materializeExample();
+
+// const newCategoryIngredient = new NewCategoryIngredient()
+// newCategoryIngredient.render()
