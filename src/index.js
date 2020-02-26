c33d457451c6ac78030e83c3de6d55fbdb6e7d7c
ilona <hellosidney17@gmail.com>
2020-02-26 11:55:59 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 // import { example, example2, materializeExample } from '@/js/example';
 
 // materializeExample();
+
+// import {header} from '@/js/header';
+// materializeHeader();
+import Header from './js/header'
+new Header();
