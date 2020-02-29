263529d2b221d3b95ec8d297a143def2662d9f8e
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-02-29 11:10:50 +0200

    some styles added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-// import { example, example2, materializeExample } from '@/js/example';
-
-// materializeExample();
-// import GoodsCollection from './js/goodsCollection/goodsCollection';
-// const collectionG = new GoodsCollection();
-// const workShop = document.querySelector('#root');
-// collectionG.start(workShop);
+import GoodsCollection from './js/goodsCollection/goodsCollection.js';
+const collectionG = new GoodsCollection();
+const workShop = document.querySelector('#root');
+collectionG.start(workShop);
