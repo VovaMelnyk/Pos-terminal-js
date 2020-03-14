80fc8b40624886600a00a8f405d61e000715585f
Diz1111Diz <kavunv@gmail.com>
2020-03-14 15:27:32 +0200

    add createNewCategoriesProduct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import LoginForm from '@/js/login-form';
-window.addEventListener('DOMContentLoaded', new LoginForm());
-import Header from './components/header/header';
+// import LoginForm from '@/js/login-form';
+// window.addEventListener('DOMContentLoaded', new LoginForm());
+// import Header from './components/header/header';
 
-const headerPart = new Header();
+// const headerPart = new Header();
+
+import createNewCategoriesProduct from './components/component-createNewCategoriesProduct/createNewCategoriesProduct';
+
+const root = document.querySelector('#root');
+const categoriesProduct = new createNewCategoriesProduct();
+categoriesProduct.start(root);
